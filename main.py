0b6f2fe56a0ef44db0e955fdddf8c50a2204040b
Aleksey K [koshi8bit] <koshi8bit@mail.ru>
2021-07-06 18:32:31 +0700

    before first start

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from dotenv import load_dotenv
+from telegram_my import TelegramMy
+import os, datetime
 import subprocess
- 
-process = subprocess.Popen(["sensors"],shell=False,stdout=subprocess.PIPE)
-data = process.communicate()    # returns tuple
-temp = int(data[0].split()[5][1:3])
-print temp
+
+if __name__ == '__main__':
+    load_dotenv()
+    teleg = TelegramMy(os.getenv('TELEGRAMTOKEN'), os.getenv('CHATID'))
+    teleg.set_project_prefix('hdd-temp')
+
+    # process = subprocess.Popen(["sudo hddtemp /dev/sda"], shell=False, stdout=subprocess.PIPE)
+    # data = process.communicate()  # returns tuple
+    # temp = int(data[0].split()[5][1:3])
+    # # print(temp)
+    # print(data[0])
+    teleg.send('wazzap')
